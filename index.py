7588a5471d126dab078c075ed1b98e5481e0ba8b
Tomi <141858115+tomiBraunn@users.noreply.github.com>
2025-04-21 11:46:02 -0300

    Terminado

--- index.py
+++ index.py
@@ -1,56 +1,42 @@
 import random
 
-disparos = 10
-barcos = 3
+N = int(input("Tamaño del tablero N (recomendamos usar como maximo 10): "))
+cantidad_barcos = int(input("Cantidad de barcos: "))
+cantidad_disparos = int(input("Cantidad de disparos: "))
 
+# Generar posiciones de barcos aleatorias
+posiciones_posibles = [(fila_tablero, columna_tablero) for fila_tablero in range(N) for columna_tablero in range(N)]
+barcos = random.sample(posiciones_posibles, cantidad_barcos)
 
-tamaño = int(input("¿De qué tamaño querés el tablero?: "))
+tablero = [["~"] * N for _ in range(N)]
 
-tablero_oculto = []
-tablero_jugador = []
+def mostrar_tablero():
+    print("  " + " ".join(str(numero_columna) for numero_columna in range(N)))
+    for numero_fila in range(N):
+        print(numero_fila, " ".join(tablero[numero_fila]))
+    print()
 
-for fila_num in range(tamaño):
-    fila_barcos = []
-    fila_visible = []
+for turno_actual in range(1, cantidad_disparos + 1):
+    print("Disparo", turno_actual, "/", cantidad_disparos)
+    mostrar_tablero()
+    fila_disparo = int(input("Fila: "))
+    columna_disparo = int(input("Columna: "))
 
-    for columna_num in range(tamaño):
-        fila_barcos += [False]
-        fila_visible += [" "]
-
-    tablero_oculto += [fila_barcos]
-    tablero_jugador += [fila_visible]
-
-barcos_colocados = 0
-while barcos_colocados < barcos:
-    fila_azar = random.randint(0, tamaño - 1)
-    columna_azar = random.randint(0, tamaño - 1)
-    if tablero_oculto[fila_azar][columna_azar] == False:
-        tablero_oculto[fila_azar][columna_azar] = True
-        barcos_colocados += 1
-
-while disparos > 0:
-    for fila in tablero_jugador:
-        print(fila)
-
-    fila_disparo = int(input("fila: "))
-    columna_disparo = int(input("columna: "))
-
-    if 0 <= fila_disparo < tamaño and 0 <= columna_disparo < tamaño:
-        if tablero_oculto[fila_disparo][columna_disparo] == True:
-            tablero_oculto[fila_disparo][columna_disparo] = False
-            tablero_jugador[fila_disparo][columna_disparo] = "X"
-            barcos -= 1
-            if barcos == 0:
-                print("Hundiste todos los barcos.")
+    if fila_disparo < 0 or fila_disparo >= N or columna_disparo < 0 or columna_disparo >= N:
+        print("Posicion no valida")
+    elif tablero[fila_disparo][columna_disparo] != "~":
+        print("Ya disparate aca")
+    else:
+        if (fila_disparo, columna_disparo) in barcos:
+            tablero[fila_disparo][columna_disparo] = "X"
+            barcos.remove((fila_disparo, columna_disparo))
+            print("Tocado")
+            if len(barcos) == 0:
+                print("Ganaste!")
                 break
         else:
-            tablero_jugador[fila_disparo][columna_disparo] = "O"
-        disparos -= 1
-        print("Te quedan", disparos, "disparos.")
+            tablero[fila_disparo][columna_disparo] = "O"
+            print("Agua")
 
-if barcos > 0:
-    print("No hundiste todos los barcos")
-
-print("Tablero final:")
-for fila in tablero_jugador:
-    print(fila)
+# Tablero final
+mostrar_tablero()
